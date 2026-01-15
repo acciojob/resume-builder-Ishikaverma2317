3528cea51528b14c0bb9f95c292d8086ec94ac13
ishikaverma17 <ishikaverma2317@gmail.com>
2026-01-15 12:28:16 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,41 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import { Provider } from "react-redux";
-import { createStore, combineReducers } from "redux";
-import App from "./App";
+import { store } from "./app/store";
+import App from "./app/rootReducer";
 
-const profile = (state = {}, action) => {
-  if (action.type === "SET_PROFILE") return action.payload;
-  return state;
-};
-
-const education = (state = [], action) => {
-  if (action.type === "ADD_EDUCATION") return [...state, action.payload];
-  return state;
-};
-
-const skills = (state = [], action) => {
-  if (action.type === "ADD_SKILL") return [...state, action.payload];
-  return state;
-};
-
-const projects = (state = [], action) => {
-  if (action.type === "ADD_PROJECT") return [...state, action.payload];
-  return state;
-};
-
-const social = (state = [], action) => {
-  if (action.type === "ADD_SOCIAL") return [...state, action.payload];
-  return state;
-};
-
-const store = createStore(
-  combineReducers({ profile, education, skills, projects, social })
-);
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
